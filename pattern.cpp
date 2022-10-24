1482dbc8ce97056e0c9ff8b6f972279257b63f37
Baba-Yaga-07 <115881621+Baba-Yaga-07@users.noreply.github.com>
2022-10-24 14:50:50 +0530

    v1

--- pattern.cpp
+++ pattern.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 int main()
-{int i,j,k,n=5;
+{int i,j,k,n;
  cout <<  "Enter Size = "<< endl;
+ cin>>n;
  for(i=1;i<=n;i++)
  {for(j=1;j<=n;j++)
    cout<<"* ";
